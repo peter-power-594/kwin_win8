14681897f696dff2a15cd22a2690479b5e8ea278
Pierre-Henri Lavigne <lavigne.pierrehenri@proton.me>
2025-10-11 07:38:22 +0900

    Update compatibility with Plasma 6 (#1)

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-
-NAME="kwin_win8"
-DATA_DIR=`kf5-config --path data | cut -d":" -f 1`
-SERVICES_DIR=`kf5-config --path services | cut -d":" -f 1`
-
-mkdir -p ${DATA_DIR}kwin/decorations/$NAME
-cp -r * ${DATA_DIR}kwin/decorations/$NAME
-cp metadata.desktop ${SERVICES_DIR}$NAME.desktop
-kbuildsycoca5 &> /dev/null
-echo kwin_win8 Installed
+if command -v "kpackagetool6"; then
+kpackagetool6 -t KWin/Decoration .
+fi
